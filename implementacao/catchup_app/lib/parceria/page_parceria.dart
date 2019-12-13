9543563c64cda9a15ad843d968f43bc7a83da6f2
peryvieira <peryvieira_@hotmail.com>
2019-12-13 04:53:11 -0300

    criacao do menu do filtro da pagina Parceria

--- implementacao/catchup_app/lib/parceria/page_parceria.dart
+++ implementacao/catchup_app/lib/parceria/page_parceria.dart
@@ -5,7 +5,27 @@ class PageParceria extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: <Widget>[],
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(left: 15.0, right: 15),
+            child: Container(
+              height: 55,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    "Todos",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  Icon(
+                    Icons.list,
+                    size: 35,
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
