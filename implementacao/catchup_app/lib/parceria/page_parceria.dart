6c3da7c83980c0752824eef86e56e359bfcac946
peryvieira <peryvieira_@hotmail.com>
2019-12-13 05:09:37 -0300

    ajeitado bug de overflow

--- implementacao/catchup_app/lib/parceria/page_parceria.dart
+++ implementacao/catchup_app/lib/parceria/page_parceria.dart
@@ -5,29 +5,34 @@ class PageParceria extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(left: 15.0, right: 15),
-            child: Container(
-              height: 55,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text(
-                    "Todos",
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  Icon(
-                    Icons.list,
-                    size: 35,
-                  ),
-                ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0, right: 15),
+              child: Container(
+                height: 55,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      "Todos",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    Icon(
+                      Icons.list,
+                      size: 35,
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          ItemParceria('assets/images/logo_parceria.png')
-        ],
+            ItemParceria('assets/images/logo_parceria.png'),
+            ItemParceria('assets/images/logo_parceria.png'),
+            ItemParceria('assets/images/logo_parceria.png'),
+            ItemParceria('assets/images/logo_parceria.png')
+          ],
+        ),
       ),
     );
   }
