a4c85a7aee9307d1bb54b515defc96f4e4beee50
peryvieira <peryvieira_@hotmail.com>
2019-12-13 06:24:07 -0300

    Inserido TextInput(Legenda) e Buttom(publicar) no Upload

--- implementacao/catchup_app/lib/upload/page_upload.dart
+++ implementacao/catchup_app/lib/upload/page_upload.dart
@@ -1,53 +1,43 @@
+import 'package:catchup_app/upload/flatButtomUpload.dart';
 import 'package:flutter/material.dart';
 import 'package:dotted_border/dotted_border.dart';
 
 class PageUpload extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(children: <Widget>[
-      Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: DottedBorder(
-            color: Colors.black,
-            strokeWidth: 1,
-            child: Container(
-              height: 300,
-              width: 300,
-              child: FlatButton(
-                  child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Container(
-                  width: 250,
-                  height: 90,
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black, width: 1.0)),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        Icon(
-                          Icons.camera_alt,
-                          size: 40,
-                        ),
-                        SizedBox(width: 20),
-                        Flexible(
-                          child: Column(
-                            children: <Widget>[
-                              Text("Tire outra foto"),
-                              Text(
-                                  "Use a sua camera para adicionar uma nova foto")
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              )),
-            )),
-      )
-    ]);
+    return SingleChildScrollView(
+      child: Column(children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            children: <Widget>[
+              DottedBorder(
+                  color: Colors.black,
+                  strokeWidth: 1,
+                  child: Container(
+                      height: 300,
+                      width: 300,
+                      child: Column(
+                        children: <Widget>[
+                          FlatButtomUpload(),
+                          FlatButtomUpload()
+                        ],
+                      ))),
+              SizedBox(height: 20),
+              Text(
+                "Adicione uma Legenda",
+                style: Theme.of(context).textTheme.title,
+              ),
+              TextField(
+                decoration:
+                    InputDecoration(hintText: "Digite Sua Legenda Aqui"),
+              ),
+              SizedBox(height: 20),
+              RaisedButton(child: Text("Publicar"))
+            ],
+          ),
+        )
+      ]),
+    );
   }
 }
